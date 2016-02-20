f0a96138c542bb835ed5f5af90a073a2184b86dc
stamaniorec <stamaniorec@gmail.com>
2016-02-20 19:28:02 +0200

    #6 Update for transactions through Ajax

--- frontend/script/transactions.js
+++ frontend/script/transactions.js
@@ -2,26 +2,33 @@ $(document).ready(function() {
     "use strict";
 
     var add_transaction_button = $("button#add-transaction-button");
-    var add_transaction_panel = $("#add-transaction-form-panel");
+    var transaction_panel = $("#transaction-form-panel");
     var transaction_submit_button = $("#transaction-submit");
 
-    var add_transaction_close = $("#add-transaction-close");
-    add_transaction_close.on('click', function() {
+    var transaction_panel_close = $("#transaction-panel-close");
+    transaction_panel_close.on('click', function() {
         add_transaction_button.show();
-        add_transaction_panel.hide();
+        transaction_panel.hide();
     });
     
-    add_transaction_panel.hide();
+    transaction_panel.hide();
 
     add_transaction_button.on('click', function() {
-        add_transaction_panel.show();
+        transaction_panel.show();
         add_transaction_button.hide();
     });
 
     transaction_submit_button.on('click', function() {
-        create_transaction();
-        add_transaction_button.show();
-        add_transaction_panel.hide();
+        var btn_text = transaction_submit_button.text();
+        if(btn_text === "Add") {
+            create_transaction();
+            add_transaction_button.show();
+            transaction_panel.hide();
+        } else if(btn_text === "Save") {
+            updateTransaction(transaction_panel.attr("data-id"));
+            transaction_panel.hide();
+            add_transaction_button.show();
+        }
     });
 
     var ENDPOINT = "http://localhost:3000/transactions";
@@ -33,18 +40,20 @@ $(document).ready(function() {
 
     date_input.datepicker();
 
-    function create_transaction() {
-        var transaction = {
+    function buildTransactionFromInputs() {
+        return {
             description: description_input.val(),
             value: value_input.val(),
             date: date_input.val(),
-            important: important_input.val()
+            important: important_input.parent().hasClass('checked')
         };
+    }
 
+    function create_transaction() {
         $.ajax(ENDPOINT, {
             method: "POST",
             contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(transaction),
+            data: JSON.stringify(buildTransactionFromInputs()),
             dataType: "json"
         }).then(function(response) {
             displayTransactions();
@@ -72,6 +81,8 @@ $(document).ready(function() {
                 .append($("<td></td>").text(transaction.description))
                 .append($("<td></td>").text(transaction.date))
                 .append($("<td></td>").text(transaction.important))
+                .append($("<td></td>")
+                                    .append($("<button class=\"btn btn-primary transaction-edit-btn\">Edit</button>")))
         );
     }
 
@@ -86,4 +97,46 @@ $(document).ready(function() {
     }
 
     displayTransactions();
+
+    function readTransaction(id) {
+        return $.ajax(ENDPOINT + "/" + id, {
+            method: "GET",
+            dataType: "json"
+        });
+    }
+
+    function updateTransaction(id) {
+        console.log(buildTransactionFromInputs());
+        console.log(important_input.val());
+        console.log(important_input);
+        return $.ajax(ENDPOINT + "/" + id, {
+            method: "PUT",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(buildTransactionFromInputs()),
+            dataType: "json"
+        });
+    }
+
+    function editTransaction(id) {
+        add_transaction_button.hide();
+        transaction_panel.attr("data-id", id);
+        transaction_panel.show();
+        transaction_panel.find('.panel-heading').text("Edit transaction");
+        transaction_panel.find('#transaction-submit').text("Save");
+
+        readTransaction(id).then(function(response) {
+            description_input.val(response.description);
+            value_input.val(response.value);
+            date_input.val(response.date);
+            if(response.important == true || response.important == "on") {
+                important_input.parent().addClass('checked');
+            } else {
+                important_input.parent().removeClass('checked');
+            }
+        });
+    }
+
+    $("tbody").on('click', '.transaction-edit-btn', function() {
+        editTransaction($(this).closest('tr').children('td').first().text())
+    })
 });
