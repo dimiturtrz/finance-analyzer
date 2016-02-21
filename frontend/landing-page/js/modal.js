7dcbb8bb4e02e584e072f5c464725dd3623ec35c
stamaniorec <stamaniorec@gmail.com>
2016-02-21 16:37:01 +0200

    #8 Sign-up implemented from front-end. Saving password in plain text though!

--- frontend/landing-page/js/modal.js
+++ frontend/landing-page/js/modal.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
     
     $('.registration-form').on('submit', function(e) {
     	
-    	$(this).find('input[type="text"], textarea').each(function(){
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
     		if( $(this).val() == "" ) {
     			e.preventDefault();
     			$(this).addClass('input-error');
