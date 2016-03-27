cabf9b99220b1f06a6b2a9edaa7842a1b495ec1d
stamaniorec <stamaniorec@gmail.com>
2016-03-27 14:59:07 +0300

    Balance compared to last month implemented. #40

--- finance-analyzer/src/main/webapp/script/stats.js
+++ finance-analyzer/src/main/webapp/script/stats.js
@@ -13,10 +13,13 @@ $(document).ready(function() {
 
     // TODO:
     // 1. Absolute income for this month - DONE
-    // 2. Absolute loss for this month
-    // 3. Absolute income - loss
-    // 4. Percentage income-loss compared to last month
+    // 2. Absolute loss for this month DONE
+    // 3. Absolute income - loss DONE
+    // 4. Percentage income-loss compared to last month DONE
     // 5. Percentage for unimportant things out of all
+
+    var balanceThisMonthDeferred = $.Deferred(),
+        balanceLastMonthDeferred = $.Deferred();
 
     getTransactions(fromThisMonth).then(function(transactions) {
         // var absoluteIncomeForThisMonth = $.Deferred(),
@@ -29,6 +32,29 @@ $(document).ready(function() {
         dashboardContentContainer.append("<br>");
         var balance = absoluteIncomeForThisMonth + absoluteLossForThisMonth;
         dashboardContentContainer.append("Balance (absolute income - loss) for this month: " + balance);
+        balanceThisMonthDeferred.resolve(balance);
+    });
+
+    getTransactions(fromLastMonth).then(function(transactions) {
+        var absoluteIncomeForLastMonth = absoluteIncome(transactions);
+        var absoluteLossForLastMonth = absoluteLoss(transactions);
+        dashboardContentContainer.append("<br>");
+        dashboardContentContainer.append("Absolute income for last month: " + absoluteIncomeForLastMonth);
+        dashboardContentContainer.append("<br>");
+        dashboardContentContainer.append("Absolute loss for last month: " + absoluteLossForLastMonth);
+        dashboardContentContainer.append("<br>");
+        var balance = absoluteIncomeForLastMonth + absoluteLossForLastMonth;
+        dashboardContentContainer.append("Balance (absolute income - loss) for this month: " + balance);
+        balanceLastMonthDeferred.resolve(balance);
+    });
+
+    $.when(
+        balanceThisMonthDeferred,
+        balanceLastMonthDeferred
+    ).then(function(balanceThisMonth, balanceLastMonth) {
+        dashboardContentContainer.append("<br>");
+        var balanceComparedToLastMonthInPercents = balanceThisMonth / balanceLastMonth * 100;
+        dashboardContentContainer.append("Balance compared to last month: " + balanceComparedToLastMonthInPercents + "%");
     });
 
     function getTransactions(filter) {
@@ -69,6 +95,19 @@ $(document).ready(function() {
             else
                 return absoluteLoss;
         }, 0);
+    }
+
+    function fromLastMonth(transaction) {
+        var curMonth = (new Date()).getMonth();
+        var curYear = (new Date()).getYear();
+
+        var transactionMonth = new Date(Date.parse(transaction.date)).getMonth();
+        var transactionYear = new Date(Date.parse(transaction.date)).getYear();
+
+        if((curMonth == 0 && transactionMonth == 11) && (curYear - transactionYear == 1))
+            return true;
+        
+        return (curMonth == transactionMonth + 1) && (curYear == transactionYear);
     }
 
     var dashboardContentContainer = $("#dashboard-panel .page-content");
