2271c10decd5977ef48d965d546c853ab65d8578
dimiturtrz <dimitar.trz@gmail.com>
2016-04-22 02:02:51 +0300

    setting up shiros' many configurations. Closes #51

--- finance-analyzer/src/main/java/entities/User.java
+++ finance-analyzer/src/main/java/entities/User.java
@@ -19,8 +19,6 @@ public class User {
 	private int id;
 	private String email;
 	private String username;
-	private String password_hash;
-	private String password_salt;
 	private String password;
 	private float balance;
 	
@@ -41,18 +39,6 @@ public class User {
 	}
 	public void setUsername(String username) {
 		this.username = username;
-	}
-	public String getPassword_hash() {
-		return password_hash;
-	}
-	public void setPassword_hash(String password_hash) {
-		this.password_hash = password_hash;
-	}
-	public String getPassword_salt() {
-		return password_salt;
-	}
-	public void setPassword_salt(String password_salt) {
-		this.password_salt = password_salt;
 	}
 	public float getBalance() {
 		return balance;
